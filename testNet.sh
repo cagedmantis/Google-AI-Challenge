8a38fb4ee7328a0c16451f31674fd2acee7558cd
Carlos Amedee <carlos.amedee@gmail.com>
2010-09-17 19:45:07 -0400

    Added the website testing ground. http://www.benzedrine.cx/planetwars/

---
+++ testNet.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+rm -fr ../testing_ai_net
+
+mkdir ../testing_ai_net
+cp *.cc ../testing_ai_net
+cp *.h ../testing_ai_net
+cp Makefile ../testing_ai_net
+
+cd ../testing_ai_net
+
+make clean
+make
+
+
+gcc -o tcp tcp.c
+
+./tcp 213.3.30.106 9999 cagedmantis -p meowmeow ./MyBot >>game_log.log
+
+
